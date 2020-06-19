2ef61f9128cba7208ad563361499edc956fb543e
bloodsoul <812566160@qq.com>
2020-06-19 16:07:18 +0800

    commit

--- app/src/main/java/com/blood/imageloader/base/HSImageLoader.java
+++ app/src/main/java/com/blood/imageloader/base/HSImageLoader.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 
+import com.blood.MainApplication;
 import com.blood.imageloader.HSImageDefaultOptions;
 
 import java.io.File;
@@ -28,6 +29,16 @@ public class HSImageLoader implements IImageStrategy {
     }
 
     /**
+     * 可以使用 Application Context（就不会监听Activity的生命周期做出变化）
+     * 不一定需要外部传入的 Context
+     *
+     * @return Context
+     */
+    private Context getApplicationContext() {
+        return MainApplication.getApplication();
+    }
+
+    /**
      * 初始化配置
      */
     public void init() {
@@ -42,7 +53,7 @@ public class HSImageLoader implements IImageStrategy {
      */
     private void setImageLoader(IImageStrategy loader) {
         if (loader == null) {
-            throw new NullPointerException("ImageLoader Strategy cannot be null.");
+            throw new IllegalStateException("ImageLoader Strategy cannot be null.");
         }
         mImageLoader = loader;
     }
@@ -71,21 +82,45 @@ public class HSImageLoader implements IImageStrategy {
         mImageLoader.load(context, option, callback);
     }
 
+    /**
+     * 同步加载图片（必须在子线程调用）
+     *
+     * @param context 上下文
+     * @param option  配置，可参考默认配置{@link HSImageDefaultOptions}
+     * @return Bitmap
+     */
     @Override
     public Bitmap loadSync(@NonNull Context context, @NonNull HSImageOption option) {
         return mImageLoader.loadSync(context, option);
     }
 
+    /**
+     * 下载图片
+     *
+     * @param context  上下文
+     * @param option   配置，可参考默认配置{@link HSImageDefaultOptions}
+     * @param callback 回调
+     */
     @Override
     public void download(@NonNull Context context, @NonNull HSImageOption option, DownloadCallback callback) {
         mImageLoader.download(context, option, callback);
     }
 
+    /**
+     * 检查是否有缓存资源，包括内存和磁盘
+     *
+     * @param context  上下文
+     * @param url      图片url
+     * @param callback 回调
+     */
     @Override
     public void checkExistCache(@NonNull Context context, String url, CheckExistCacheCallback callback) {
         mImageLoader.checkExistCache(context, url, callback);
     }
 
+    /**
+     * 清除内存缓存
+     */
     @Override
     public void clearMemoryCache() {
         if (mImageLoader != null) {
@@ -93,6 +128,9 @@ public class HSImageLoader implements IImageStrategy {
         }
     }
 
+    /**
+     * 清除磁盘缓存
+     */
     @Override
     public void clearDiskCache() {
         if (mImageLoader != null) {
@@ -100,9 +138,17 @@ public class HSImageLoader implements IImageStrategy {
         }
     }
 
+    public void load(Uri uri, Callback callback) {
+        load(getApplicationContext(), uri, callback);
+    }
+
     public void load(Context context, Uri uri, Callback callback) {
         HSImageOption option = new HSImageOption.Builder().uri(uri).build();
         load(context, option, callback);
+    }
+
+    public void load(File file, Callback callback) {
+        load(getApplicationContext(), file, callback);
     }
 
     public void load(Context context, File file, Callback callback) {
@@ -110,9 +156,17 @@ public class HSImageLoader implements IImageStrategy {
         load(context, option, callback);
     }
 
+    public void load(String url, Callback callback) {
+        load(getApplicationContext(), url, callback);
+    }
+
     public void load(Context context, String url, Callback callback) {
         HSImageOption option = new HSImageOption.Builder().url(url).build();
         load(context, option, callback);
+    }
+
+    public void load(@DrawableRes int resourceId, Callback callback) {
+        load(getApplicationContext(), resourceId, callback);
     }
 
     public void load(Context context, @DrawableRes int resourceId, Callback callback) {
@@ -120,9 +174,17 @@ public class HSImageLoader implements IImageStrategy {
         load(context, option, callback);
     }
 
+    public Bitmap loadSync(Uri uri) {
+        return loadSync(getApplicationContext(), uri);
+    }
+
     public Bitmap loadSync(Context context, Uri uri) {
         HSImageOption option = new HSImageOption.Builder().uri(uri).build();
         return loadSync(context, option);
+    }
+
+    public Bitmap loadSync(File file) {
+        return loadSync(getApplicationContext(), file);
     }
 
     public Bitmap loadSync(Context context, File file) {
@@ -130,9 +192,17 @@ public class HSImageLoader implements IImageStrategy {
         return loadSync(context, option);
     }
 
+    public Bitmap loadSync(String url) {
+        return loadSync(getApplicationContext(), url);
+    }
+
     public Bitmap loadSync(Context context, String url) {
         HSImageOption option = new HSImageOption.Builder().url(url).build();
         return loadSync(context, option);
+    }
+
+    public Bitmap loadSync(@DrawableRes int resourceId) {
+        return loadSync(getApplicationContext(), resourceId);
     }
 
     public Bitmap loadSync(Context context, @DrawableRes int resourceId) {
@@ -140,24 +210,64 @@ public class HSImageLoader implements IImageStrategy {
         return loadSync(context, option);
     }
 
+    public void display(ImageView view, Uri uri) {
+        display(getApplicationContext(), view, uri);
+    }
+
     public void display(Context context, ImageView view, Uri uri) {
         display(context, view, uri, null);
+    }
+
+    public void display(ImageView view, File file) {
+        display(getApplicationContext(), view, file);
     }
 
     public void display(Context context, ImageView view, File file) {
         display(context, view, file, null);
     }
 
+    public void display(ImageView view, String url) {
+        display(getApplicationContext(), view, url);
+    }
+
     public void display(Context context, ImageView view, String url) {
         display(context, view, url, null);
+    }
+
+    public void display(ImageView view, Bitmap bitmap) {
+        display(getApplicationContext(), view, bitmap);
     }
 
     public void display(Context context, ImageView view, Bitmap bitmap) {
         display(context, view, bitmap, null);
     }
 
+    public void display(ImageView view, @DrawableRes int resourceId) {
+        display(getApplicationContext(), view, resourceId);
+    }
+
     public void display(Context context, ImageView view, @DrawableRes int resourceId) {
         display(context, view, resourceId, null);
+    }
+
+    public void display(ImageView view, Uri uri, HSImageOption option) {
+        display(getApplicationContext(), view, uri, option);
+    }
+
+    public void display(ImageView view, File file, HSImageOption option) {
+        display(getApplicationContext(), view, file, option);
+    }
+
+    public void display(ImageView view, String url, HSImageOption option) {
+        display(getApplicationContext(), view, url, option);
+    }
+
+    public void display(ImageView view, @DrawableRes int resourceId, HSImageOption option) {
+        display(getApplicationContext(), view, resourceId, option);
+    }
+
+    public void display(ImageView view, Bitmap bitmap, HSImageOption option) {
+        display(getApplicationContext(), view, bitmap, option);
     }
 
     public void display(Context context, ImageView view, Uri uri, HSImageOption option) {
